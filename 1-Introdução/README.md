e5bb263ba5216fc6da37818e0dec3af8cc7763a1
Naira da Silva <naira.andrade35@gmail.com>
2024-10-24 18:22:43 -0300

    Status 1

---
+++ 1-Introdução/README.md
@@ -0,0 +1,7 @@
+## Introdução à linguagem Java 
+
+ #####  **_Imprimir mengem na tela_**
+
+~~~java
+ system.out.println ("Olá,mundo !"); 
+~~~~
---
+++ 1-Introdução/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Saída 
+        System.out.println("Olá, mundo !");
+    }
+}
